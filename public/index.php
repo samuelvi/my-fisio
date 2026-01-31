9488fe47a70385b138d60e214388b213ab755aef
Samuel <samuel.vicent@takeachef.com>
2026-01-31 23:41:58 +0100

    fix(prod): add IONOS/Apache compatibility patch for JWT Authorization header

--- public/index.php
+++ public/index.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 use App\Kernel;
 
+// IONOS/Apache compatibility fix for JWT
+if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+    $_SERVER['HTTP_AUTHORIZATION'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
